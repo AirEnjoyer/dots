770b5aef86e7f42de5479ddfba5d862dad85fc36
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-04 21:33:19 -0700

    Updated Neovim Config

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,3 +4,4 @@ vim.cmd "set softtabstop=4"
 vim.cmd "set shiftwidth=4"
 
 vim.keymap.set("n", "<leader><S-n>", "<cmd>Neogit<CR>")
+vim.api.nvim_set_keymap("n", "<F2>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -39,7 +39,6 @@ return {
         },
         mappings = {
             n = {
-
                 ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
                 ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
