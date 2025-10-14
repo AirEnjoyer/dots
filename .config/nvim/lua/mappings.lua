1c08fd461c2bb54d6653d11a73d7262e9f26e59d
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-14 14:53:53 -0700

    Updated, omarchy focused now

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,6 +4,5 @@ vim.cmd "set softtabstop=4"
 vim.cmd "set shiftwidth=4"
 
 vim.keymap.set("n", "<leader><S-n>", "<cmd>Neogit<CR>")
-vim.api.nvim_set_keymap("n", "<F2>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<C-P>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F2>", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-P>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
