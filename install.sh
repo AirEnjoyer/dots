f60d0c42d7ae8f614711f6a54913f2a5e0f35eec
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-15 13:42:50 -0700

    Updated install.sh, added glad (openGL), and README.md

--- install.sh
+++ install.sh
@@ -1,21 +1,45 @@
-#install paru
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/paru.git
-cd ~/paru
-makepkg -si
-cd
+cp -r ~/dots/.config/* ~/.config
+cp ~/dots/glad ~/
 
-#install dependencies
-paru -S wl-clipboard ttf-cascadia-code-nerd luarocks tmux tree alacritty starship stow zen-browser-bin lxappearance discord 7zip blueman-git gnome-tweaks fzf zoxide okular mirage mpv ninja clang tmux npm zen zsh
-
-sudo cp -r ~/dots/.config/* ~/.config
-
-
-
-sudo cp -r ~/dots/.zshrc ~/
+cp -r ~/dots/.zshrc ~/
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-source ~/.bashrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+source ~/.zshrc
 hyprctl reload
-sudo cp ~/dots/CMakeLists.txt ~/
 
+yay -S superfile
+yay -S starship
+mkdir ~/repos
+cd ~/repos
+git clone --depth 1 --branch main --single-branch https://github.com/AirEnjoyer/clone
+cd clone/build
+cmake .. && ninja
+cp qclone /usr/local/bin
+cd ~/repos
+rm -rf clone
+qclone AirEnjoyer/template
+qclone AirEnjoyer/sdltemplate
+cd template/build
+cmake .. && ninja
+cp template /usr/local/bin
+cd ~/repos
+rm -rf template
+cd sdltemplate/build
+cmake .. && ninja
+cp sdltemplate /usr/local/bin/
+cd
+omarchy-theme-install https://github.com/AirEnjoyer/omarchy-ashen-theme
+cd ~/repos/
+rm -rf sdltemplate
+qclone AirEnjoyer/mkfat
+cd mkfat/build
+cmake .. && ninja
+cp qclone /usr/local/bin/
+cd ~/repos/
+rm -rf qclone
+qclone AirEnjoyer/txt2pdf
+cd txt2pdf/build
+cmake .. && ninja
+cp txt2pdf /usr/local/bin/
