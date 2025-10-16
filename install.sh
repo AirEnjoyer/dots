14cad3f18dc0cc15b4a3306d3e04238a8b1801b8
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-15 17:58:44 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -11,35 +11,14 @@ hyprctl reload
 
 yay -S superfile
 yay -S starship
-mkdir ~/repos
-cd ~/repos
-git clone --depth 1 --branch main --single-branch https://github.com/AirEnjoyer/clone
-cd clone/build
-cmake .. && ninja
-cp qclone /usr/local/bin
-cd ~/repos
-rm -rf clone
-qclone AirEnjoyer/template
-qclone AirEnjoyer/sdltemplate
-cd template/build
-cmake .. && ninja
-cp template /usr/local/bin
-cd ~/repos
-rm -rf template
-cd sdltemplate/build
-cmake .. && ninja
-cp sdltemplate /usr/local/bin/
-cd
-omarchy-theme-install https://github.com/AirEnjoyer/omarchy-ashen-theme
-cd ~/repos/
-rm -rf sdltemplate
-qclone AirEnjoyer/mkfat
-cd mkfat/build
-cmake .. && ninja
-cp qclone /usr/local/bin/
-cd ~/repos/
-rm -rf qclone
-qclone AirEnjoyer/txt2pdf
-cd txt2pdf/build
-cmake .. && ninja
-cp txt2pdf /usr/local/bin/
+git clone https://github.com/AirEnjoyer/linuxUtils
+cd linuxUtils
+g++ install.cpp -o install
+mkdir build
+cd build
+../install mkfat
+../install qclone 
+../install sdltemplate
+../install template
+../install tobin
+../install txt2pdf
