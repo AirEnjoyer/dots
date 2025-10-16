f9f96207dfdea3ddc4af13ebb0fbe7fcea8bedb3
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-16 11:48:19 -0700

    Added oil.nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,5 +1,6 @@
 return {
     "AstroNvim/astrocommunity",
+    { import = "astrocommunity.file-explorer.oil-nvim" },
     { import = "astrocommunity.pack.lua" },
     { import = "astrocommunity.pack.cmake" },
 }
