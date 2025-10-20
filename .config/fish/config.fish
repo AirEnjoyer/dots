d69f059963264b88676852fac3687b6cb9e7f8bd
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-20 16:26:34 -0700

    Added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,60 @@
+set -gx ZSH "$HOME/.oh-my-zsh"
+set -gx ZSH_THEME "robbyrussell"
+set -gx PATH "$HOME/scripts" $PATH
+set -gx SDL_VIDEODRIVER wayland
+set -gx CC clang
+set -gx CXX clang++
+set -gx CMAKE_GENERATOR Ninja
+set -gx HYPRSHOT_DIR "$HOME/Screenshots"
+
+fish_vi_key_bindings
+
+alias lg='lazygit'
+alias f='clear; fastfetch'
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+function spf
+    set os (uname -s)
+    if test "$os" = Linux
+        set -gx SPF_LAST_DIR "$XDG_STATE_HOME/superfile/lastdir"
+        if not test -n "$XDG_STATE_HOME"
+            set -gx SPF_LAST_DIR "$HOME/.local/state/superfile/lastdir"
+        end
+    end
+    command spf $argv
+    if test -f "$SPF_LAST_DIR"
+        source "$SPF_LAST_DIR"
+        rm -f "$SPF_LAST_DIR" > /dev/null
+    end
+end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    set cwd ""
+    yazi $argv --cwd-file="$tmp"
+    if test -f "$tmp"
+        set cwd (cat "$tmp")
+        if test -n "$cwd" -a "$cwd" != "$PWD"
+            cd "$cwd"
+        end
+        rm -f "$tmp"
+    end
+end
+
+function run
+    if test (count $argv) -eq 0
+        echo "Usage: run <program> [args...]"
+        return 1
+    end
+    set app $argv[1]
+    set args $argv[2..-1]
+    ninja; and ./"$app" $args
+end
+
+starship init fish | source
+zoxide init fish | source
+if not contains "$HOME/.local/bin" $PATH
+    set -gx PATH "$HOME/.local/bin" $PATH
+end
+    set -U fish_greeting ""
