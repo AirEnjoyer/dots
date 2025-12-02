3436dc87689ba0588989952c01ce6bffd71d1cbd
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-12-02 15:36:58 -0800

    Improved install/distro/config.sh

--- install/void/configs.sh
+++ install/void/configs.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-need_stow=true
+clean-configs(){
+    for dir in ~/dots/.config/*; do 
+        if [ -L ~/.config/$(basename $dir) ]; then 
+            unlink ~/.config/$(basename $dir)
+        elif [ -e ~/.config/$(basename $dir) ]; then
+            rm ~/.config/$(basename $dir)
+        fi; 
+    done
+}
 
+read -p "Do you want to remove anything in the way (CANNONT BE UNDONE UNLESS YOU HAVE A BACKUP))? (y/n): " choice
 
-for dir in ~/dots/.config/*; do 
-    if [ -L ~/.config/$(basename $dir) ]; then 
-            echo "Must unlink ~/.config/$(basename $dir)"; 
-            need_stow=false
-    elif [ -e ~/.config/$(basename $dir) ]; then
-            echo "Must remove ~/.config/$(basename $dir)"; 
-            need_stow=false
-    fi; 
-done
-if $need_stow; then
-        echo "run 'cd ~/dots; stow .'"
+if [[ "$choice" =~ ^[yY]$ ]]; then
+    clean-configs
+    (cd ~/dots; stow .)
+else
+    for dir in ~/dots/.config/*; do 
+        if [ -L ~/.config/$(basename $dir) ]; then 
+                echo "Must unlink ~/.config/$(basename $dir)"; 
+                need_stow=false
+        elif [ -e ~/.config/$(basename $dir) ]; then
+                echo "Must remove ~/.config/$(basename $dir)"; 
+                need_stow=false
+        fi; 
+    done
 fi
