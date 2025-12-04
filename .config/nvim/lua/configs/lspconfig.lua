003ad0ab9f374faebf951eeb3ed040a64d4d572e
wokerNM <nicholasmilligan2019@outlook.com>
2025-12-03 22:06:51 -0800

    Added neocmake to lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,12 +1,12 @@
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
-
 local lspconfig = require "nvchad.configs.lspconfig"
 
 lspconfig.servers = {
   "lua_ls",
   "clangd",
+  "neocmake",
 }
 
 local default_servers = {
