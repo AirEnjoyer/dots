236a572cf6532d10ef4126f45fdec78c38b74cfd
Woker <nicholasmilligan2019@outlook.com>
2025-12-05 10:27:41 -0800

    Update options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,8 +9,21 @@ local function indent_by_num(count)
   end
 end
 
+local function unindent_by_num(count)
+  count = count or 1 
+  for i = 1, count do
+    vim.cmd('normal! <<')  
+  end
+end
+
+--Tab + 1-9 to indent by 1-9
 vim.api.nvim_set_keymap('n', '<Tab>', ':lua indent_by_num(v:count)<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<Tab>', ':lua indent_by_num(v:count)<CR>', { noremap = true, silent = true })
+
+
+--Shift-Tab + 1-9 to unindent by 1-9
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':lua unindent_by_num(v:count)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<S-Tab>', ':lua unindent_by_num(v:count)<CR>', { noremap = true, silent = true })
 
 o.shiftwidth = 4
 o.tabstop = 4
