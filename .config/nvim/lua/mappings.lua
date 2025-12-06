6630e47227810b0403c5107293e3fc41f43104e6
wokerNM <nicholasmilligan2019@outlook.com>
2025-12-05 17:14:04 -0800

    Remove unnecessary mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,24 +9,6 @@ map("n", ";", ":", { noremap = true, desc = "Enter command mode without shift" }
 map("n", "<leader><Tab>", ":bnext<CR>", { desc = "Next Buffer" })
 map("n", "<leader><S-Tab>", ":bprevious<CR>", { desc = "Previous bufffer" })
 
-local ls = require "luasnip"
-
-vim.keymap.set({ "i" }, "<C-K>", function()
-  ls.expand()
-end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-L>", function()
-  ls.jump(1)
-end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-J>", function()
-  ls.jump(-1)
-end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<C-E>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
-end, { silent = true })
-
 map("i", "jk", "<ESC>")
 
 M.dap = {
