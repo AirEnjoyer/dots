006cc7537a5ac719f35e87bd5304e01651d4619a
wokerNM <nicholasmilligan2019@outlook.com>
2025-12-14 16:52:05 -0800

    updated keymap for oil to not be fullscreen

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 local map = vim.keymap.set
 
-vim.keymap.set("n", "<Leader>e", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+vim.keymap.set("n", "<Leader>e", "<cmd>Oil<CR>", { desc = "Open Parent Directory in Oil" })
 map("n", "<Leader>ff", '<cmd>lua require("fzf-lua").files()<CR>', { desc = "fzf through files" })
 
 map("n", "<leader>w", ":w<CR>", { desc = "Save" })
