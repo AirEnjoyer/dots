46c3aa7fc35afbc8717777c7d68958cedc08dd4b
wokerNM <nicholasmilligan2019@outlook.com>
2025-12-15 17:08:15 -0800

    Added some neovim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 local map = vim.keymap.set
-
-vim.keymap.set("n", "<Leader>e", "<cmd>Oil<CR>", { desc = "Open Parent Directory in Oil" })
+map("n", "<Leader>e", "<cmd>Oil<CR>", { desc = "Open Parent Directory in Oil" })
 map("n", "<Leader>ff", '<cmd>lua require("fzf-lua").files()<CR>', { desc = "fzf through files" })
 
 map("n", "<leader>w", ":w<CR>", { desc = "Save" })
@@ -16,11 +15,15 @@ map("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle Break" })
 map("n", "<Leader>dc", dap.continue, { desc = "Continue" })
 map("n", "<Leader>dr", dap.repl.open, { desc = "Inspect" })
 map("n", "<Leader>dk", dap.terminate, { desc = "Kill" })
-
 map("n", "<Leader>dso", dap.step_over, { desc = "Step Over" })
 map("n", "<Leader>dsi", dap.step_into, { desc = "Step Into" })
 map("n", "<Leader>dsu", dap.step_out, { desc = "Step Out" })
 map("n", "<Leader>dl", dap.run_last, { desc = "Run Last" })
+
+vim.api.nvim_set_keymap("i", "<A-h>", "<Left>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-l>", "<Right>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-j>", "<Down>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-k>", "<Up>", { noremap = true, silent = true })
 
 local dapui = require("dapui")
 map("n", "<Leader>duu", dapui.open, { desc = "Open UI" })
@@ -28,17 +31,17 @@ map("n", "<Leader>duc", dapui.close, { desc = "Close UI" })
 
 local ls = require("luasnip")
 
-vim.keymap.set({ "i" }, "<C-K>", function()
+map({ "i" }, "<C-S-K>", function()
 	ls.expand()
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-L>", function()
+map({ "i", "s" }, "<C-S-l>", function()
 	ls.jump(1)
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-J>", function()
+map({ "i", "s" }, "<C-S-J>", function()
 	ls.jump(-1)
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<C-E>", function()
+map({ "i", "s" }, "<C-E>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
